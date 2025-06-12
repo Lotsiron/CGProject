8b5d103b848a8673e853a9ec963b7863e063ad7a
MicheleZazzaretti7 <117993014+MicheleZazzaretti7@users.noreply.github.com>
2025-06-12 22:39:55 +0200

    Update CG2025-Variables.cpp

--- CG2025-Variables.cpp
+++ CG2025-Variables.cpp
@@ -6,3 +6,13 @@ SDL_Surface* screen = NULL;
 
 SDL_Color palette8[screenWidth*screenHeight];
 int howManyColours = 0;
+Uint8 mode = 0;
+Uint8 dithering = 0;
+Uint8 compression = 0;
+int datasize =0;
+
+int BayerTable4[4][4] = {{6, 14,  8, 16},
+                        {10,  2, 12,  4},
+                        { 7, 15,  5, 13},
+                        {11,  3,  9,  1}};
+float updatedBayerTable4[4][4];
